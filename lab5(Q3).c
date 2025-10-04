4861d914a53b5db569562e1cb401c86190e4b7a7
k250615-Amna <k250615@nu.edu.pk>
2025-10-04 20:37:11 +0500

    Update lab5(Q3).c

--- lab5(Q3).c
+++ lab5(Q3).c
@@ -5,34 +5,35 @@ each, choose brand (Phone: 1=Samsung, 2=Apple; Laptop: 1=Dell, 2=HP).*/
 int main(){
 	char device;
 	char brand;
-	printf("Select between a laptop(L) or a phone(P)");
+	printf("Select any between a laptop(L) or a phone(P)");
 	scanf("%c" , &device);
 	
 	switch(device){
 		case 'P':
-			printf("Choose brand samsung(S) or Apple(A)");
+			printf("Choose a brand samsung(S) or Apple(A)");
 			scanf(" %c" , &brand);
 			switch(brand){
 				case 'S':
-					printf("You choose a samsung phone");
+					printf("You selected an samsung phone");
 					break;
 				case 'A':
-					printf("You choose a apple phone");
+					printf("You selected an apple phone");
 					break;	
 			}
 			break;
 		case 'L':
-			printf("Choose brand from Dell(D) or HP(H)");
+			printf("Select any brand from Dell(D) or HP(H)");
 			scanf(" %c" , &brand);
 			switch(brand){
 				case 'D':
-					printf("You choose a Dell laptop");
+					printf("You selected an Dell laptop");
 					break;
 				case 'H':
-					printf("You choose a HP Laptop");
+					printf("You selected an HP Laptop");
 					break;
 			}
 			break;
 	}
 	return 0;
 }
+
